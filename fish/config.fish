60abd8ea83fc2479ec663c9ff075b29c6af8d44a
Diplo <tmoynat@gmail.com>
2019-09-29 01:56:04 +0200

    term.sh

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,13 @@
+set PATH /snap/bin $PATH
+set XDG_DATA_DIRS $PATH
+
+alias ne=nano
+alias ls=exa
 alias cat=bat
+
+function prompt_loc --on-event fish_prompt
+    echo $PWD > /tmp/whereami
+end
 
 if status is-login
 	if test -z "$DISPLAY" -a $XDG_VTNR = 1
--- rofi_patch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-bg_color=#001A2B
-text_color=#f3f4f5
-htext_color=#9f78e1
-
-rofi -show run -lines 7 -location 1 -yoffset 27 -eh 2 -width 10 -padding 5 -click-to-exit --opacity 80 -bw 0 -color-window "$bg_color, $bg_color, $bg_color" -color-normal "$bg_color, $text_color, $bg_color, $bg_color, $htext_color" -font "System San Francisco Display 12"

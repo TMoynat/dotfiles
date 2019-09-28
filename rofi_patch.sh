565eabdf16e7c9cb61e759eb2502ea44e6121daa
Diplo <tmoynat@gmail.com>
2019-09-28 21:09:58 +0200

    new

--- rofi_patch.sh
+++ rofi_patch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-bg_color=#282A36
+bg_color=#001A2B
 text_color=#f3f4f5
-htext_color=#9575cd
+htext_color=#9f78e1
 
-rofi -show run -lines 3 -eh 2 -width 100 -padding 800 --opacity 80 -bw 0 -color-window "$bg_color, $bg_color, $bg_color" -color-normal "$bg_color, $text_color, $bg_color, $bg_color, $htext_color" -font "System San Francisco Display 16"
+rofi -show run -lines 7 -location 1 -yoffset 27 -eh 2 -width 10 -padding 5 -click-to-exit --opacity 80 -bw 0 -color-window "$bg_color, $bg_color, $bg_color" -color-normal "$bg_color, $text_color, $bg_color, $bg_color, $htext_color" -font "System San Francisco Display 12"
